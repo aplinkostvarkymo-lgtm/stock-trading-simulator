e950ffe748c8f4f329e7a08eccb145f2e01e0383
Robertas <aplinkostvarkymo@gmail.com>
2026-01-06 20:18:30 +0200

    Added Zapier Chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Script
+          src="https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js"
+          strategy="lazyOnload"
+          type="module"
+        />
+        <zapier-interfaces-chatbot-embed 
+          is-popup="true" 
+          chatbot-id="cmk2to07x001anu002k2etvd8"
+        />
+      </body>
     </html>
   )
 }
